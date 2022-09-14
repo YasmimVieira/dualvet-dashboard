cec9fe5430b9ea42fffd10b3a045e83ad40ab6dc
YasmimVieira <vieiray937@gmail.com>
2022-09-14 19:34:42 -0300

    feature: create components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { GraphComponent } from './components/graph/graph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    CardsComponent,
+    GraphComponent
   ],
   imports: [
     BrowserModule,
