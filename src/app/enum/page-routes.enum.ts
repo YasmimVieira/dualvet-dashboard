d6a152f4b029714592d10ef71da802081f003c89
YasmimVieira <vieiray937@gmail.com>
2022-09-29 21:44:55 -0300

    feature: add login

--- src/app/enum/page-routes.enum.ts
+++ src/app/enum/page-routes.enum.ts
@@ -1,4 +1,5 @@
 export enum PageRoutes {
     PRODUCTS_ROUTER = 'products',
-    AUTHENTICATION = 'authentication'
+    AUTHENTICATION = 'authentication',
+    SIGNUP = 'signup'
 }
--- src/app/modules/authentication/authentication-routing.module.ts
+++ src/app/modules/authentication/authentication-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageRoutes } from 'src/app/enum/page-routes.enum';
 import { AuthenticationComponent } from './authentication.component';
+import { SigninupComponent } from './signinup/signinup.component';
 
-const routes: Routes = [{ path: '', component: AuthenticationComponent }];
+const routes: Routes = [
+  { path: '', component: AuthenticationComponent },
+  { path: PageRoutes.SIGNUP, component: SigninupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
